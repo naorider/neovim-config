952f99051e8d11d523e84836f6a44e3b4de80659
Naoki <435569+naorider@users.noreply.github.com>
2026-06-14 22:38:54 +0900

    Update treesitter and hop

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,29 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master", -- Use the legacy version.
+    lazy = false,
     build = ":TSUpdate",
-    init = function()
-      require("nvim-treesitter.configs").setup({
-        -- A list of parser names, or "all" (the four listed parsers should always be installed)
-        ensure_installed = {
-          "lua",
-          "vim",
-          "html",
-          "javascript",
-          "typescript",
-          "tsx",
-        },
+    config = function()
+      require("nvim-treesitter").install({
+        "lua",
+        "vim",
+        "html",
+        "javascript",
+        "typescript",
+        "tsx",
+      })
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-        highlight = { enable = true },
-        -- Disable the experimental feature
-        -- indent = { enable = true }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "lua", "vim", "html", "javascript", "typescript", "typescriptreact" },
+        callback = function()
+          -- Highlighting
+          vim.treesitter.start()
+          -- Folding
+          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo[0][0].foldmethod = "expr"
+          -- Indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
       })
     end,
   },
